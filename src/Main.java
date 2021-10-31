0c1e36c7a9fc2b829774213733548d5b0e8dd9b4
Andrey Gordeev <goral656@yandex.ru>
2021-10-31 08:54:54 +0300

    commit 1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 400;
+        int rublesForOneBonusMile = 20;
+        int bonusMiles = ticketPrice / rublesForOneBonusMile;
+        System.out.println(bonusMiles);
+
+    }
+}
